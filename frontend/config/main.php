fcf944a4eec721d38a295b584cb2d398c6fd2fd9
ewwgit <support@expertwebworx.com>
2017-01-23 18:10:13 +0530

    seo urls

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -1,11 +1,12 @@
 <?php
+use \yii\web\Request;
 $params = array_merge(
     require(__DIR__ . '/../../common/config/params.php'),
     require(__DIR__ . '/../../common/config/params-local.php'),
     require(__DIR__ . '/params.php'),
     require(__DIR__ . '/params-local.php')
 );
-
+$baseUrl = str_replace('/frontend/web', '', (new Request)->getBaseUrl());
 return [
     'id' => 'app-frontend',
     'basePath' => dirname(__DIR__),
@@ -18,8 +19,19 @@ return [
 		],
     'components' => [
         'request' => [
-            'csrfParam' => '_csrf-frontend',
-        ],
+    				'baseUrl' => $baseUrl,
+    				'enableCsrfValidation' => false,
+    		],
+    		'urlManager' => [
+    				'baseUrl' => $baseUrl,
+    				'enablePrettyUrl' => true,
+    				'showScriptName' => false,
+    				'rules' => [
+    						'vendors' => 'vendor/vendor/index',
+    						
+    						
+    				]
+    		],
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
